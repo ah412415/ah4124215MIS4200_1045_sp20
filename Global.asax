29d162003f7dda2b8bcf6de95e29c3cfa8054238
Hiatt <ah412415@ohio.edu>
2020-01-22 11:40:06 -0500

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ah4124215MIS4200_1045_sp20.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ah4124215MIS4200_1045_sp20.Startup))]
+namespace ah4124215MIS4200_1045_sp20
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
